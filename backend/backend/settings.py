2c692f98c1cff73d59a382830add9767a9ed278d
Kamil Kaźmierczak <kamilkazmierczak10@gmail.com>
2025-04-29 17:43:28 +0200

    Config postgrsql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
+from decouple import config
+import dj_database_url
 import os
 
 load_dotenv()
@@ -95,10 +97,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse(
+        config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+        )
 }
 
 
