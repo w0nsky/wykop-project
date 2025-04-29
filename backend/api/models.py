d811e54d9116df7a7f764053b6534f6f2b4a7f11
Kamil Kaźmierczak <kamilkazmierczak10@gmail.com>
2025-04-29 18:40:31 +0200

    Category model migrate

--- backend/api/models.py
+++ backend/api/models.py
@@ -19,7 +19,9 @@ class Post(models.Model):
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        null=True,
+        blank=True
     )
     title = models.CharField(max_length=255)
     content = models.TextField()
